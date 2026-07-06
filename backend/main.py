2b74cb643e2c2c684be1a6313c0988d932e910ce
Raza-learner <mohammadrazashaikh59@gmail.com>
2026-07-06 15:41:03 +0530

    diagnose: add debug middleware to trace CORS headers

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import Response
 from app.core.logger import logger
 from app.api.upload import router as upload_router
 from app.api.health import router as health_router
@@ -12,12 +15,24 @@ app = FastAPI(
     version="1.0.0",
 )
 
+class DebugCORSHeadersMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        response = await call_next(request)
+        if "origin" in request.headers:
+            response.headers["X-Debug-CORS-Origin"] = request.headers["origin"]
+            response.headers["X-Debug-Allow-Origin"] = response.headers.get("access-control-allow-origin", "NOT_SET")
+        return response
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
+app.add_middleware(
+    DebugCORSHeadersMiddleware,
 )
 
 logger.info("Bill Extractor API started")
